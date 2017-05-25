40ce5b9c1d8166e5fac0b990d366ea96fa22d0a5
zhuyakk <smallcat552001@hotmail.com>
2017-05-25 20:57:34 +0800

    [Request] 1.0.0 add send logic
    Signed-off-by: zhuyakk <smallcat552001@hotmail.com>

---
+++ app/src/main/java/com/android/chatplan/Const.java
@@ -0,0 +1,11 @@
+package com.android.chatplan;
+
+/**
+ * Created by pc on 2017/5/25.
+ */
+
+public class Const {
+    public static final String SERVER_IP = "47.93.236.31";
+    public static final int SERVER_PORT = 8888;
+
+}
--- app/src/main/java/com/android/chatplan/MainActivity.kt
+++ app/src/main/java/com/android/chatplan/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.android.chatplan
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.net.Socket
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(){
+
+    private var mEditView :EditText? = null
+    private var mSendView :TextView? = null
+    private var mListView :RecyclerView ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar) as Toolbar)
+        if (supportActionBar != null) {
+            supportActionBar?.title = ""
+        }
+
+        mEditView = findViewById(R.id.edit) as EditText
+        mSendView = findViewById(R.id.send) as TextView
+        mListView = findViewById(R.id.content) as RecyclerView
+
+        mSendView!!.setOnClickListener(View.OnClickListener {
+            if (mEditView!!.text == null){
+                return@OnClickListener
+            }
+
+            Thread(Runnable {
+                var sendMessage = mEditView!!.text.toString()
+                var socket = Socket(Const.SERVER_IP, Const.SERVER_PORT)
+                var writer = BufferedWriter(OutputStreamWriter(
+                        socket.getOutputStream()))
+                if (writer != null){
+                    writer.write(sendMessage)
+                    writer.flush()
+                    socket.shutdownOutput()
+                    writer.close()
+                    socket.close()
+                }
+                mSendView!!.post {
+                    mEditView!!.text = null
+                    Toast.makeText(this, R.string.send_successful, Toast.LENGTH_SHORT).show()
+                }
+            }).start()
+        })
     }
 }
